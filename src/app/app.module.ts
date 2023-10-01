0e7128a3f16be8799f60c218612a71d992d22ee0
fernando <isaacfernando5351@gmail.com>
2023-09-30 20:28:04 -0600

    product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { RecoveryPasswordComponent } from './pages/recoveryPassword/recovery-pas
 import { GetComponent } from './pages/category/get/get.component';
 import { CreateCategoryComponent } from './pages/category/create/create.component';
 import { CreateProductComponent } from './pages/product/create/create.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import { CreateProductComponent } from './pages/product/create/create.component'
     GetComponent,
     CreateProductComponent,
     CreateCategoryComponent,
+    ProductComponent,
   ],
   imports: [
     BrowserModule,
